b6e905d2ffaabbb906edf7d2620752d76408759c
Chris Ross (ASP.NET) <chrross@microsoft.com>
2018-02-02 10:53:51 -0800

    Add ClaimActions for bulk add and remove. #1609

--- src/Microsoft.AspNetCore.Authentication.OAuth/Claims/ClaimActionCollectionMapExtensions.cs
+++ src/Microsoft.AspNetCore.Authentication.OAuth/Claims/ClaimActionCollectionMapExtensions.cs
@@ -88,6 +88,26 @@ namespace Microsoft.AspNetCore.Authentication
         }
 
         /// <summary>
+        /// Maps all values from the json user data as claims, excluding duplicates.
+        /// </summary>
+        /// <param name="collection"></param>
+        public static void MapAll(this ClaimActionCollection collection)
+        {
+            collection.Add(new MapAllClaimsAction());
+        }
+
+        /// <summary>
+        /// Maps all values from the json user data as claims, excluding the specified types.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="exclusions"></param>
+        public static void MapAllExcept(this ClaimActionCollection collection, params string[] exclusions)
+        {
+            collection.Add(new MapAllClaimsAction());
+            collection.DeleteClaims(exclusions);
+        }
+
+        /// <summary>
         /// Delete all claims from the given ClaimsIdentity with the given ClaimType.
         /// </summary>
         /// <param name="collection"></param>
@@ -95,6 +115,24 @@ namespace Microsoft.AspNetCore.Authentication
         public static void DeleteClaim(this ClaimActionCollection collection, string claimType)
         {
             collection.Add(new DeleteClaimAction(claimType));
+        }
+
+        /// <summary>
+        /// Delete all claims from the ClaimsIdentity with the given claimTypes.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="claimTypes"></param>
+        public static void DeleteClaims(this ClaimActionCollection collection, params string[] claimTypes)
+        {
+            if (claimTypes == null)
+            {
+                throw new ArgumentNullException(nameof(claimTypes));
+            }
+
+            foreach (var claimType in claimTypes)
+            {
+                collection.Add(new DeleteClaimAction(claimType));
+            }
         }
     }
 }
